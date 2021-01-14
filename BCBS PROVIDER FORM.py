45338ba5f6a61d981d5721ca5081363b1431334a
gzavala86 <49504978+gzavala86@users.noreply.github.com>
2021-01-13 20:33:45 -0600

    Update BCBS PROVIDER FORM.py
    added code  for switching to new window and for asserting that headline logo text matched html text.

--- BCBS PROVIDER FORM.py
+++ BCBS PROVIDER FORM.py
@@ -1,7 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+import time
 
 driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
 
 driver.get("https://www.bcbsil.com/provider/network/join_bcbsil_network.html")
+##driver.implicitly_wait(15)
 driver.find_element_by_link_text("Provider Onboarding Form").click()
-driver.implicitly_wait(10)
+time.sleep(10)
+pw = driver.window_handles[02]
+childwindow = driver.window_handles[1]
+
+driver.switch_to.window(childwindow)
+driver.find_element_by_css_selector("div[role='dialog']")
+driver.find_element_by_css_selector("svg.wm-ignore-css-reset").click()
+
+logo = driver.find_element_by_class_name("sr-only").text
+assert logo == "Blue Cross and Blue Shield of Illinois"
+print(logo)
+driver.close()
+driver.switch_to_window(pw)
+driver.close()
+
+##driver.find_element_by_id("488bfe6a-64a7-f480-4f66-27ea42a687a5").click()
